1c353e7077cd41e80818597a3a7e16af354d82a6
Davis Jordan <davisridleyjordan@gmail.com>
2020-08-30 23:32:47 -0400

    Parse page of specified url

--- scraper.py
+++ scraper.py
@@ -4,10 +4,9 @@ from bs4 import BeautifulSoup
 # set user agent and source url
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
-URL = 'http://stockx.com/air-jordan-1-retro-high-satin-snake-chicago-w'
+URL = 'https://stockx.com/search/sneakers?s=Jordan%201%20Retro%20High%20Satin%20Snake%20Chicago'
 
 # send request for url
 page = requests.get(URL, headers=headers)
 
-# print page html
-print(page.text)
+soup = BeautifulSoup(page.content, 'html.parser')
